a99b11f1399ff3f6516b3169a56dd5e95e1d6367
Pam VanLeirsburg <pvanleirsburg@barrington220.org>
2026-05-06 12:56:52 -0500

    Completed through number 18

--- Pig/ContentView.swift
+++ Pig/ContentView.swift
@@ -40,7 +40,29 @@ struct ContentView: View {
             }
         }
     }
-}
+    func endTurn() {
+        turnScore = 0
+        randomValue = 0
+    }
+    func chooseRandom (times: Int) {
+        if times > 0 {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                randomValue = Int.random(in: 1...6)
+                chooseRandom(times: times - 1)
+            }
+        }
+        if times == 0 {
+            if randomValue == 1 {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    endTurn()
+                }
+            }
+            else {
+                turnScore += randomValue
+            }
+        }
+    }
+    
 
 struct CustomText: View {
     let text: String
