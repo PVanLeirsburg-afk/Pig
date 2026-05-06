61e60a343083d8a0754ac673ec7d5fcd9e5827aa
Pam VanLeirsburg <pvanleirsburg@barrington220.org>
2026-05-06 12:22:21 -0500

    Initial Commit

---
+++ Pig/PigApp.swift
@@ -0,0 +1,17 @@
+//
+//  PigApp.swift
+//  Pig
+//
+//  Created by Pamela VanLeirsburg on 5/6/26.
+//
+
+import SwiftUI
+
+@main
+struct PigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
